c194934c02cc776dd24a3715fd31c61f220b5182
namesty <nestor09amesty@gmail.com>
2021-07-08 04:54:37 +0200

    Lowered memory sized and bumped package version

--- packages/wasm/as/assembly/asyncify.ts
+++ packages/wasm/as/assembly/asyncify.ts
@@ -1,4 +1,4 @@
-const asyncifyStorage = new ArrayBuffer(10000 * 1024);
+const asyncifyStorage = new ArrayBuffer(100 * 1024);
 
 export function w3_asyncify_storage(): u32 {
   return changetype<u32>(asyncifyStorage);
