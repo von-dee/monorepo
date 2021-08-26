bc84835438952a5609902c9d19f14328c166cc24
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-08-26 15:01:11 +0000

    Implement `InterfaceImplementations` without `Tracing`

--- packages/rs/core/src/types/interface_implementations.rs
+++ packages/rs/core/src/types/interface_implementations.rs
@@ -1,5 +1,4 @@
 use super::Uri;
-use polywrap_tracing_rs::Tracer;
 
 #[derive(Clone, Debug)]
 pub struct InterfaceImplementations {
@@ -7,16 +6,17 @@ pub struct InterfaceImplementations {
     implementations: Vec<Uri>,
 }
 
-pub fn sanitize_interface_implementations(_tracer: Tracer) {
-    // TODO: let input = tracer.trace_func(args: T, span: &'static str, func: fn(args: T) -> Result<T, Error>);
-    let input: Vec<InterfaceImplementations> = vec![];
+pub fn sanitize_interface_implementations(
+    input: &[InterfaceImplementations],
+) -> Result<Vec<InterfaceImplementations>, &str> {
     let mut output: Vec<InterfaceImplementations> = vec![];
     for definition in input {
         let interface_uri = Uri::new(&definition.interface.get_uri());
-        let implementations = definition.implementations;
+        let implementations = &definition.implementations;
         output.push(InterfaceImplementations {
             interface: interface_uri,
-            implementations,
+            implementations: implementations.to_vec(),
         });
     }
+    Ok(output)
 }
