87d6463d2074ff07b8d08c98b3d7ac35e2c680ee
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-08-26 20:41:53 +0000

    Implement `parse_uri` for the core `Uri` type

--- packages/rs/core/src/types/uri.rs
+++ packages/rs/core/src/types/uri.rs
@@ -1,6 +1,7 @@
 //! URI type
 
 // use tracing::Tracer when it's implemented
+use regex::Regex;
 
 /// URI configuration
 #[derive(Clone, Debug, Default, PartialEq)]
@@ -60,7 +61,57 @@ impl Uri {
         }
     }
 
-    pub fn parse_uri(_uri: &str) -> Result<UriConfig, &str> {
-        todo!()
+    pub fn parse_uri(uri: &str) -> Result<UriConfig, String> {
+        if uri.as_bytes().is_empty() {
+            return Err("The provided URI is empty".to_string());
+        }
+        let mut processed = uri.to_string();
+
+        // Trim preceding '/' characters
+        // Why are we using a `while` loop here?
+        while processed.starts_with('/') {
+            processed = processed.chars().nth(1).unwrap().to_string();
+        }
+        // Check for the w3:// scheme, add if it isn't there
+        // let w3_scheme_idx = processed.find("w3://").unwrap();
+        // If it's missing the w3:// scheme, add it
+        if !processed.contains("w3://") {
+            processed = format!("{}{}", "w3://", processed);
+        }
+
+        // If the w3:// is not in the beginning, throw an error
+        if !processed.starts_with("w3://") {
+            return Err("The w3:// scheme must be at the beginning of the URI string".to_string());
+        }
+
+        // Extract the authority & path
+        let mut result = String::from("");
+        let re = Regex::new(r"w3://([a-z][a-z0-9-_]+)/(.*)")
+            .unwrap()
+            .find(&processed);
+
+        // Remove whitespaces
+        if let Ok(re) = Regex::new(r"w3://([a-z][a-z0-9-_]+)/(.*)") {
+            result = re.as_str().chars().filter(|c| !c.is_whitespace()).collect()
+        } else if re.is_none() || re.unwrap().as_str().len() != 3 {
+            let uri_received = format!("Invalid URI Received: {}", uri);
+            let error = format!(
+                "{}\n{}\n{}\n{}\n\n{}",
+                "URI is malformed, here are some examples of valid URIs:",
+                "w3://ipfs/QmHASH",
+                "w3://ens/domain.eth",
+                "ens/domain.eth",
+                uri_received
+            );
+            return Err(error);
+        }
+        Ok(UriConfig {
+            uri: Some(processed.to_string()),
+            //authority: result[1],
+            //path: result[2],
+            // ^FIXME
+            authority: Some(result.clone()),
+            path: Some(result),
+        })
     }
 }
