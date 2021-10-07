d33b38e775c6a1d4f827251c048482dffe75c07f
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-10-07 12:10:35 +0000

    Init `migrate` function

--- packages/rs/core/src/manifest/formats/web3api/migrate.rs
+++ packages/rs/core/src/manifest/formats/web3api/migrate.rs
@@ -1,3 +1,20 @@
+use std::collections::HashMap;
+
+use super::migrators::{
+    prealpha_001_1_to_prealpha_001_5::migrate as migrate_prealpha_001_1_to_prealpha_001_5,
+    prealpha_001_2_to_prealpha_001_5::migrate as migrate_prealpha_001_2_to_prealpha_001_5,
+    prealpha_001_3_to_prealpha_001_5::migrate as migrate_prealpha_001_3_to_prealpha_001_5,
+    prealpha_001_4_to_prealpha_001_5::migrate as migrate_prealpha_001_4_to_prealpha_001_5,
+};
 use super::{
     AnyWeb3ApiManifest, Web3ApiManifest, Web3ApiManifestFormats, LATEST_WEB3_API_MANIFEST_FORMAT,
 };
+
+pub type Migrator = HashMap<&'static str, fn(old: &mut AnyWeb3ApiManifest) -> Web3ApiManifest>;
+
+pub fn migrate_web3_api_manifest(
+    manifest: &mut AnyWeb3ApiManifest,
+    to: &mut Web3ApiManifestFormats,
+) -> Result<Web3ApiManifest, String> {
+    todo!()
+}
--- packages/rs/core/src/manifest/formats/web3api/mod.rs
+++ packages/rs/core/src/manifest/formats/web3api/mod.rs
@@ -12,6 +12,11 @@ pub use prealpha_001_3::Web3ApiManifest as Web3ApiManifest001Prealpha3;
 pub use prealpha_001_4::Web3ApiManifest as Web3ApiManifest001Prealpha4;
 pub use prealpha_001_5::Web3ApiManifest as Web3ApiManifest001Prealpha5;
 
+pub type Web3ApiManifest = Web3ApiManifest001Prealpha5;
+
+pub const LATEST_WEB3_API_MANIFEST_FORMAT: Web3ApiManifestFormats =
+    Web3ApiManifestFormats::Prealpha001_5("0.0.1-prealpha.5");
+
 pub enum Web3ApiManifestFormats {
     Prealpha001_1(&'static str),
     Prealpha001_2(&'static str),
@@ -20,18 +25,13 @@ pub enum Web3ApiManifestFormats {
     Prealpha001_5(&'static str),
 }
 
-pub enum AnyWeb3ApiManifest {
-    Web3ApiManifest001Prealpha1(Web3ApiManifest001Prealpha1),
-    Web3ApiManifest001Prealpha2(Web3ApiManifest001Prealpha2),
-    Web3ApiManifest001Prealpha3(Web3ApiManifest001Prealpha3),
-    Web3ApiManifest001Prealpha4(Web3ApiManifest001Prealpha4),
-    Web3ApiManifest001Prealpha5(Web3ApiManifest001Prealpha5),
+pub struct AnyWeb3ApiManifest {
+    pub web3_api_manifest_001_prealpha1: Web3ApiManifest001Prealpha1,
+    pub web3_api_manifest_001_prealpha2: Web3ApiManifest001Prealpha2,
+    pub web3_api_manifest_001_prealpha3: Web3ApiManifest001Prealpha3,
+    pub web3_api_manifest_001_prealpha4: Web3ApiManifest001Prealpha4,
+    pub web3_api_manifest_001_prealpha5: Web3ApiManifest001Prealpha5,
 }
-
-pub type Web3ApiManifest = Web3ApiManifest001Prealpha5;
-
-pub const LATEST_WEB3_API_MANIFEST_FORMAT: Web3ApiManifestFormats =
-    Web3ApiManifestFormats::Prealpha001_5("0.0.1-prealpha.5");
 
 #[derive(Clone, Debug)]
 pub struct Modules {
