c21a6b54ec6ddd9ccf5e6031ad696a381d200050
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-10-07 02:19:23 +0000

    `0.0.1-prealpha.4.ts` ported to Rust

--- packages/rs/core/src/manifest/formats/web3api/mod.rs
+++ packages/rs/core/src/manifest/formats/web3api/mod.rs
@@ -1,3 +1,4 @@
 pub mod prealpha_001_1;
 pub mod prealpha_001_2;
 pub mod prealpha_001_3;
+pub mod prealpha_001_4;
---
+++ packages/rs/core/src/manifest/formats/web3api/prealpha_001_4.rs
@@ -0,0 +1,57 @@
+#[derive(Clone, Debug)]
+pub struct Web3ApiManifest {
+    pub format: String,
+    pub repository: Option<String>,
+    pub build: Option<String>,
+    pub language: String,
+    pub modules: Modules,
+    pub import_redirects: Option<Vec<ImportRedirects>>,
+    pub __type: String,
+}
+
+impl Default for Web3ApiManifest {
+    fn default() -> Web3ApiManifest {
+        Web3ApiManifest {
+            format: "0.0.1-prealpha.4".to_string(),
+            repository: None,
+            build: None,
+            language: "".to_string(),
+            modules: Modules::default(),
+            import_redirects: None,
+            __type: "Web3ApiManifest".to_string(),
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+struct Modules {
+    mutation: Option<Mutation>,
+    query: Option<Query>,
+}
+
+impl Default for Modules {
+    fn default() -> Modules {
+        Modules {
+            mutation: None,
+            query: None,
+        }
+    }
+}
+
+#[derive(Clone, Debug)]
+enum Mutation {
+    Schema { schema: String },
+    Module { module: Option<String> },
+}
+
+#[derive(Clone, Debug)]
+enum Query {
+    Schema { schema: String },
+    Module { module: Option<String> },
+}
+
+#[derive(Clone, Debug)]
+struct ImportRedirects {
+    uri: String,
+    schema: String,
+}
