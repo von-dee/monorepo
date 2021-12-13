5d12c6f12d2109abdf014aa3a1463e107cfe627d
Kobby Pentangeli <kobbypentangeli@gmail.com>
2021-12-13 12:08:41 +0000

    Implement manifest schema validation for `web3api_meta` format

--- packages/rs/core/src/manifest/formats/web3api_meta/mod.rs
+++ packages/rs/core/src/manifest/formats/web3api_meta/mod.rs
@@ -6,6 +6,7 @@ use serde::{Deserialize, Serialize};
 
 pub use migrate::migrate_meta_manifest;
 pub use prealpha_001_1::MetaManifest as MetaManifest001Prealpha1;
+pub use validate::validate_web3_api_manifest;
 
 pub type MetaManifest = MetaManifest001Prealpha1;
 
@@ -30,6 +31,7 @@ impl MetaManifestFormats {
     }
 }
 
+#[derive(Debug, Clone)]
 pub enum AnyMetaManifest {
     MetaManifest001Prealpha1(MetaManifest001Prealpha1),
 }
--- packages/rs/core/src/manifest/formats/web3api_meta/validate.rs
+++ packages/rs/core/src/manifest/formats/web3api_meta/validate.rs
@@ -1,1 +1,62 @@
-// TODO
+use serde_json;
+use std::collections::HashMap;
+use std::fs;
+
+use super::AnyMetaManifest;
+use valico::json_schema;
+
+type MetaManifestSchemas = HashMap<&'static str, serde_json::Value>;
+
+fn generate_schemas() -> MetaManifestSchemas {
+    let schema_0_0_1_prealpha_1_path =
+        "../../../../../../packages/manifest-schemas/formats/web3api.meta/0.0.1-prealpha.1.json";
+
+    let schema_0_0_1_prealpha_1: serde_json::Value =
+        serde_json::from_str(&fs::read_to_string(schema_0_0_1_prealpha_1_path).unwrap()).unwrap();
+
+    let mut schemas: MetaManifestSchemas = HashMap::new();
+
+    let _ = schemas.insert("0.0.1-prealpha.1", schema_0_0_1_prealpha_1);
+    schemas
+}
+
+pub fn validate_web3_api_manifest(
+    manifest: &AnyMetaManifest,
+    ext_schema: Option<serde_json::Value>,
+) -> Result<(), String> {
+    let manifest_format = AnyMetaManifest::get_manifest_format(manifest);
+    let schemas = generate_schemas();
+    let schema = schemas.get(manifest_format.as_str());
+    if schema.is_none() {
+        return Err(format!(
+            "Unrecognized MetaManifest schema format {}\nmanifest: {:#?}",
+            manifest_format, manifest
+        ));
+    }
+
+    let mut scope = json_schema::Scope::new();
+    let manifest_schema = match manifest {
+        AnyMetaManifest::MetaManifest001Prealpha1(one) => {
+            serde_json::json!(one)
+        }
+    };
+    let scoped_schema = scope
+        .compile_and_return(schema.unwrap().to_owned(), true)
+        .ok()
+        .expect("Validation errors encountered while sanitizing MetaManifest format");
+    if !scoped_schema.validate(&manifest_schema.clone()).is_valid() {
+        return Err("Got an invalid schema".to_string());
+    }
+
+    if ext_schema.is_some() {
+        let scoped_schema = scope
+            .compile_and_return(ext_schema.unwrap().to_owned(), true)
+            .ok()
+            .expect("Validation errors encountered while sanitizing ext schema");
+        if !scoped_schema.validate(&manifest_schema).is_valid() {
+            return Err("Invalid ext schema".to_string());
+        }
+    }
+
+    Ok(())
+}
